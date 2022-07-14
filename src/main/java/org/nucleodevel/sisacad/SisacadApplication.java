2d7f7e77096d90e9d0b9ea1eb6d29b079bf9a170
dallan.reis <dallan.reis@unesp.br>
2022-07-13 21:08:33 -0300

    Primeira versão com Spring Boot.

---
+++ src/main/java/org/nucleodevel/sisacad/SisacadApplication.java
@@ -0,0 +1,13 @@
+package org.nucleodevel.sisacad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisacadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisacadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nucleodevel/sisacad/SisacadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nucleodevel.sisacad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisacadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
