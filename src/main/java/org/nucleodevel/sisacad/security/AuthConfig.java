c7c27cd841e80c57cdd683458eee1946d28184c2
Dallan Augusto Toledo Reis <dallan.reis@unesp.br>
2022-09-25 17:31:09 -0300

    Evitando deprecation.

--- src/main/java/org/nucleodevel/sisacad/security/AuthConfig.java
+++ src/main/java/org/nucleodevel/sisacad/security/AuthConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@SuppressWarnings("deprecation")
 @Configuration
 public class AuthConfig extends WebSecurityConfigurerAdapter {
 
