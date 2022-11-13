719234665822c68bf19427ade7d39f7140657085
Dallan Augusto Toledo Reis <dallan.reis@unesp.br>
2022-11-12 22:58:59 -0300

    Obtendo turma por oferta de curso e discente.

--- src/main/java/org/nucleodevel/sisacad/resources/TurmaResource.java
+++ src/main/java/org/nucleodevel/sisacad/resources/TurmaResource.java
@@ -3,11 +3,13 @@ package org.nucleodevel.sisacad.resources;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import org.nucleodevel.sisacad.domain.Discente;
 import org.nucleodevel.sisacad.domain.OfertaCurso;
 import org.nucleodevel.sisacad.domain.Turma;
 import org.nucleodevel.sisacad.dto.OfertaDisciplinaDto;
 import org.nucleodevel.sisacad.dto.TurmaDto;
 import org.nucleodevel.sisacad.security.Role;
+import org.nucleodevel.sisacad.services.DiscenteService;
 import org.nucleodevel.sisacad.services.OfertaCursoService;
 import org.nucleodevel.sisacad.services.OfertaDisciplinaService;
 import org.nucleodevel.sisacad.services.TurmaService;
@@ -28,6 +30,8 @@ public class TurmaResource extends AbstractResource<Turma, TurmaDto, Integer, Tu
 	private OfertaCursoService ofertaCursoService;
 	@Autowired
 	private OfertaDisciplinaService ofertaDisciplinaService;
+	@Autowired
+	private DiscenteService discenteService;
 
 	@Override
 	public List<Role> getListAllowedRoleToRead() {
@@ -113,6 +117,34 @@ public class TurmaResource extends AbstractResource<Turma, TurmaDto, Integer, Tu
 
 		validatePermissionsToWrite();
 		return deleteSubList(id, itemId, ofertaDisciplinaService);
+	}
+
+	@RequestMapping(value = "/oferta-curso/{ofertaCursoId}", method = RequestMethod.GET)
+	public ResponseEntity<TurmaDto> findByOfertaCurso(@PathVariable Integer ofertaCursoId)
+			throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException {
+
+		validatePermissionsToRead();
+
+		OfertaCurso ofertaCurso = ofertaCursoService.find(ofertaCursoId);
+		Turma entity = ofertaCurso == null ? null : ofertaCurso.getTurma();
+
+		TurmaDto dto = entity == null ? null : getDtoFromEntity(entity);
+		return ResponseEntity.ok().body(dto);
+	}
+
+	@RequestMapping(value = "/discente/{discenteId}", method = RequestMethod.GET)
+	public ResponseEntity<TurmaDto> findByDiscente(@PathVariable Integer discenteId)
+			throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException {
+
+		validatePermissionsToRead();
+
+		Discente discente = discenteService.find(discenteId);
+		Turma entity = discente == null ? null : discente.getVestibulando().getOfertaCurso().getTurma();
+
+		TurmaDto dto = entity == null ? null : getDtoFromEntity(entity);
+		return ResponseEntity.ok().body(dto);
 	}
 
 }
